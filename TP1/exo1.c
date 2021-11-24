99ef0edb6ec5a56aab67bc08e77ade718417482d
lucley64 <54866919+lucley64@users.noreply.github.com>
2021-11-24 14:49:30 +0100

    Add files via upload

---
+++ TP1/exo1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void) {
+	int pid = getpid();
+
+	printf("Je suis le processus numéro : %d\n", pid);
+
+	pid = fork();
+
+	printf("Fork m'a renvoyé la valeur : %d\n", pid);
+
+	printf("Je suis le processus numéro : %d %s %d \n", getpid(), ", et mon père est le processus numéro", getppid());
+	
+	return 0;
+}
