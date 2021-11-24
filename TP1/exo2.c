fe44294c598e9be6afa70951cbd680f85660d6cc
lucley64 <54866919+lucley64@users.noreply.github.com>
2021-11-24 15:02:58 +0100

    exo2 fin

--- TP1/exo2.c
+++ TP1/exo2.c
@@ -7,13 +7,15 @@ int main(void){
 
     switch (pid){
         case 0:
-            for (int i = 0; i<10; i++){
+            for (int i = 1; i<10; i++){
                 printf("Nombre pairs : %d \n", (i*2));
+                //sleep(1); //Le fils s'execute et le père après
             }
             break;
         default:
-            for (int i = 0; i<10; i++){
+            for (int i = 1; i<10; i++){
                 printf("Nombre impairs : %d \n", (i*(2+1)));
+                sleep(1); //le fils commence à s'executer puis s'arrête le temps que le père se termine
             }
             break;
     }
