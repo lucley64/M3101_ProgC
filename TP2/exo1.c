444a73cf8ec81eb60005bb408bac6fc298a2dd0a
lucley64 <54866919+lucley64@users.noreply.github.com>
2021-12-01 16:45:51 +0000

    exo1

---
+++ TP2/exo1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void thread1(){
+	while(1){
+		printf(".\n");
+		sleep(1);
+	}
+	pthread_exit(NULL);
+}
+
+void thread2(){
+	printf("tapez un charactère : \n");
+	char i;
+	scanf("%c", &i);
+	pthread_exit(&i);
+}
+
+int main(void){
+        pthread_attr_t attr;
+        pthread_t id1, id2;
+
+        pthread_attr_init(&attr);
+        pthread_create(&id1, &attr, (void*)thread1, NULL);
+        pthread_create(&id2, &attr, (void*)thread2, NULL);
+
+        pthread_join(id1, NULL);
+        pthread_join(id2, NULL);
+
+        exit(0);
+}
+
