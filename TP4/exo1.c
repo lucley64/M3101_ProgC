757a5cd6f91d977cea1f3ff1cfe28c84b369edc0
lucley64 <luc.leydert@gmail.com>
2022-01-03 20:42:59 +0100

    fin exo1

--- TP4/exo1.c
+++ TP4/exo1.c
@@ -1,19 +1,45 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <ctype.h>
 
-int main(void){
-    pid_t pid = fork(void);
 
+int main(){
+    int charAEnvoyer[2];
+    char unChar;
+
+    if(pipe(charAEnvoyer)==-1){
+        printf("création du tube impossible\n");
+        return -1;
+    }
+
+
+    pid_t pid = fork();
+    
+    
     switch (pid){
+        case -1:
+            printf("création du fils impossible\n");
+            return -1;
         case 0:
-            printf("Processus Fils");
-            return 0;
+            close(charAEnvoyer[1]);
+            while(read(charAEnvoyer[0], &unChar, sizeof(char))!=0){
+                printf("Le caractere saisit est %c\n", toupper(unChar));
+            }
+            printf("Fin du fils\n");
+            close(charAEnvoyer[0]);
             break;
 
         default:
-            printf("Processus Pere");
-            return 0;
+            close(charAEnvoyer[0]);
+            unChar = getchar();
+            while(!(unChar=='$')){
+                write(charAEnvoyer[1], &unChar, sizeof(char));
+                unChar = getchar();
+            }
+            printf("Fin du pere\n");
+            close(charAEnvoyer[1]);
             break;
     }
 
